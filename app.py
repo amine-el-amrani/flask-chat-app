aa2cb9fad944144180c75fdc78499b9d2a37ffec
El amrani Amine <amine_el_amrani@outlook.com>
2024-06-23 13:11:47 +0200

    chore: Update chat route to pass username and room parameters

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def chat():
     username = request.args.get('username')
     room = request.args.get('room')
     if username and room:
-        return render_template('chat.html')
+        return render_template('chat.html', username=username, room=room)
     else:
         return redirect(url_for('home'))
 
