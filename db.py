2883e0917466f2c9011fe7b9683d4b45874f435f
El amrani Amine <amine_el_amrani@outlook.com>
2024-06-24 20:10:56 +0200

    feat: Add get_user function to retrieve user data

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
 import os
+from user import User
 from dotenv import load_dotenv
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash
@@ -14,3 +15,7 @@ users_collection = chat_db.get_collection("users")
 def save_user(username, email, password):
     password_hash = generate_password_hash(password)
     users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
